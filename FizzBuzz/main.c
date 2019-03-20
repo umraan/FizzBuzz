fa3ec5e4a7a35002ad0d0123e6c513d68e2e5d0d
umraan <umraanahmed@gmail.com>
2019-03-20 13:30:24 -0400

    FizzBuzz

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -8,8 +8,18 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void){
+    
+    int i;
+    for(i=1; i<101; i++) {
+        if(i % 15 ==0)
+            printf("FizzBuzz\n");
+        else if(i % 3 ==0)
+            printf("Fizz\n");
+        else if(i % 5 ==0)
+            printf("Buzz\n");
+        else
+            printf("%d\n", i);
+    }
     return 0;
 }
